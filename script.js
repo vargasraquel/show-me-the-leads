adfcf80fc426569c9f030c6831ed084bb7311b58
vargasraquel <58276070+vargasraquel@users.noreply.github.com>
2020-06-29 22:54:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Model
+            // Model
             function Question(stem, choices, answer) {
                 this.stem = stem;
                 this.choices = choices;
@@ -111,17 +111,6 @@
 
               // Get the modal
               var modal = document.getElementById("myModal");
-
-              // Get the button that opens the modal
-              var btn = document.getElementById("myBtn");
-
-              // Get the <span> element that closes the modal
-              var span = document.getElementsByClassName("close")[0];
-
-              // When the user clicks on <span> (x), close the modal
-              span.onclick = function() {
-              modal.style.display = "none";
-              }
 
               // When the user clicks on submit, close the modal
               function endModal() {
